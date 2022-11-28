d5706ad75285f14358979a18cdc3abec6eaa8ee4
David-Jesse <djesse351@gamil.com>
2022-11-28 08:10:30 -0800

    higher level programming

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 word = "Holberton"
-word_first_3 = Hol
-word_last_2 = on
-middle_word = olberto
+word_first_3 = "Hol"
+word_last_2 = "on"
+middle_word = "olberto"
 print(f"First 3 letters: {word_first_3}")
 print(f"Last two letterers: {word_last_2}")
 print(f"Middles word: {middle_word}")
